e1adc938cce0593d6c4260f5a5c064ebd857ec40
vikaputinceva <vikaputin2008@gmail.com>
2026-03-16 13:02:30 +0700

    init2

--- public/index.php
+++ public/index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'hello';
+declare(strict_types=1);
+
+try {
+    $app = require_once __DIR__ . '/../core/bootstrap.php';
+    $app->run();
+} catch (\Throwable $exception) {
+    echo '<pre>';
+    print_r($exception);
+    echo '</pre>';
+}
