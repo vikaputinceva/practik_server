f75c4144782d7b109f4768ddf421cfec6ba7ff76
vikaputinceva <vikaputin2008@gmail.com>
2026-03-23 12:13:52 +0700

    Using models in a project

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'mvc',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'rjspryic_m4',
+    'username' => 'rjspryic',
+    'password' => 'gXmiTc',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
