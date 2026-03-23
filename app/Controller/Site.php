766b08c20286c6eab427d24d2fff7df9784d56d5
vikaputinceva <vikaputin2008@gmail.com>
2026-03-23 12:47:57 +0700

    Processing request data

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -4,17 +4,25 @@ namespace Controller;
 
 use Model\Post;
 use Src\View;
-
+use Src\Request;
+use Model\User;
 class Site
 {
-    public function index(): string
+    public function index(Request $request): string
     {
-        $posts = Post::all();
+        $posts = Post::where('id', $request->get('id'))->get();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
     public function hello(): string
     {
-        return new View('site.hello', ['message' => 'hello working']);
+        return (new View())->render('site.hello', ['message' => 'hello working']);
+    }
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
     }
 }
--- core/Src/Route.php
+++ core/Src/Route.php
@@ -25,6 +25,9 @@ class Route
     {
         $path = explode('?', $_SERVER['REQUEST_URI'])[0];
         $path = substr($path, strlen(self::$prefix) + 1);
+        $path = TRIM($path, '/');
+
+
 
         if (!array_key_exists($path, self::$routes)) {
             throw new Error('This path does not exist');
@@ -42,6 +45,6 @@ class Route
         }
 
 
-        call_user_func([new $class, $action]);
+        call_user_func([new $class, $action], new Request());
     }
 }
