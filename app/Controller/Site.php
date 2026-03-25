29db430beaaef36115619b65195fddee49734059
vikaputinceva <vikaputin2008@gmail.com>
2026-03-25 10:20:14 +0700

    Singleton pattern

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -6,6 +6,7 @@ use Model\Post;
 use Src\View;
 use Src\Request;
 use Model\User;
+use Src\Auth\Auth;
 class Site
 {
     public function index(Request $request): string
@@ -24,5 +25,24 @@ class Site
             app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+    public function login(Request $request): string
+    {
+        //Если просто обращение к странице, то отобразить форму
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        //Если удалось аутентифицировать пользователя, то редирект
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        //Если аутентификация не удалась, то сообщение об ошибке
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
